f2c6b92d95755fa3246e5dc638d2ee400443c2ce
William <alveswlm@gmail.com>
2019-04-10 00:07:30 -0300

    Adiciona variáveis de ambiente

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 
 const server = require('http').Server(app);
-const io = require('socket.io'(server));
+const io = require("socket.io")(server);
 
 io.on("connection", socket => {
     socket.on('connectRoom', box => {
@@ -34,4 +34,4 @@ app.use('/files', express.static(path.resolve(__dirname, "..", "tmp")));
 
 app.use(require('./routes'));
 
-server.listen(3333);
+server.listen(process.env.PORT || 3333);
